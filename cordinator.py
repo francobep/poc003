0bedd4e1a7a984413c98db325602eadb1346d779
Franco Giovanolli <franco.giovanolli@cba.gov.ar>
2020-03-24 19:01:12 -0300

    .

--- cordinator.py
+++ cordinator.py
@@ -49,7 +49,8 @@ def get_connections(host):
         s.close()
         data = str(rbytes).replace("b'", "").replace("'", "").replace(':','').replace('\\n\\n', '').split('\\n')
         datalength = len(data) - 1
-
+        print(str(data))
+        exit(1)
         i = 0
         connections = []
         for line in data:
