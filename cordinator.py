1f5676c185f6008373d694fe1c89615fede48965
Franco Giovanolli <franco.giovanolli@cba.gov.ar>
2020-03-26 16:04:04 -0300

    .

--- cordinator.py
+++ cordinator.py
@@ -230,8 +230,8 @@ def tcp_sessions_and_load():
                     ###pesos 
     
     if wait:
-        print("Waiting 30s to renew connections...")
-        sleep(30)
+        print("Waiting 120s to renew connections...")
+        sleep(120)
         for worker in worker_with_conn:
             worker = worker[0]
             set_server_state(worker,"ready")
