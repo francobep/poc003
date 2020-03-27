80606c7b4ed6c5a82c042617b0c5790a97ae0ab7
Franco Giovanolli <franco.giovanolli@cba.gov.ar>
2020-03-26 21:20:31 -0300

    .

--- cordinator.py
+++ cordinator.py
@@ -19,7 +19,7 @@ ch = logging.StreamHandler()
 ch.setLevel(logging.DEBUG)
 
 # create formatter
-formatter = logging.Formatter("%(asctime)s {0} {1} %(levelname)s: %(message)s")
+formatter = logging.Formatter("%(asctime)s %(levelname)s: %(message)s")
 
 # add formatter to ch
 ch.setFormatter(formatter)
