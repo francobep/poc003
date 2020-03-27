30817dbb78ea1ed1c1fbbd242b7a1be4ac647472
Franco Giovanolli <franco.giovanolli@cba.gov.ar>
2020-03-26 22:06:16 -0300

    .

--- cordinator.py
+++ cordinator.py
@@ -98,18 +98,17 @@ def get_traffic(host, connection):
 def get_connections(host):
     connections = []
     rdata = send_to_socket(host,"show sess")
-    print(str(rdata))
-    exit(1)
-    data = str(rdata[0]).replace(":","")
-    
-    datalength = len(data) - 1
+    datalength = len(rdata) - 1
     print("Conections: "+ str(datalength))
     i = 0
     connections = []
-    for line in data:
+    for line in rdata:
         if datalength > i:
             line = line.split(' ')
-            id = line[0]
+            id = str(line[0]).replace(":","")
+            print(str(id))
+            exit(1)
+
             traffic = get_traffic(host,id)
             connections.append([id,traffic])
             i = i + 1
