2abebd756de6d6a77f506918ac8a0b1dda9b718d
Franco Giovanolli <franco.giovanolli@cba.gov.ar>
2020-03-29 06:21:18 -0300

    ∏

--- cordinator.py
+++ cordinator.py
@@ -10,6 +10,7 @@ import six
 from kubernetes import client, config
 
 logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 ch = logging.StreamHandler()
 ch.setLevel(logging.INFO)
 formatter = logging.Formatter('[%(asctime)s] %(pathname)s:%(lineno)d %(funcName)s %(levelname)s - %(message)s','%m-%d %H:%M:%S')
