d92735fcfae5ed5c7e339ff3b240cfa908be981f
Franco Giovanolli <franco.giovanolli@cba.gov.ar>
2020-04-01 03:36:50 -0300

    .

--- cordinator.py
+++ cordinator.py
@@ -348,7 +348,6 @@ def tcp_sessions(sleeptime=3, lbmode=1, dryrun=False):
             for conn in connections:
                 conn_traffic = conn[1]
                 worker_traffic = worker_traffic + conn_traffic
-                total_traffic = total_traffic - worker_traffic
                 logging.debug(conn_traffic)
                 logging.debug(worker_traffic)
             rest_total_traffic = total_traffic - worker_traffic
