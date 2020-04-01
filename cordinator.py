56a047f59b561d19c1ca2431ed7161969ff9b10e
Franco Giovanolli <franco.giovanolli@cba.gov.ar>
2020-04-01 09:19:35 -0300

    .

--- cordinator.py
+++ cordinator.py
@@ -287,7 +287,8 @@ def get_stats(workers):
     for worker_with_con in workers_with_conn:
         for worker in worker_with_con:
             if worker != ['']:
-                logging.info("Worker => " + worker[0] + " has " + str(len(worker[1])) + " connections")
+                print(str(worker))
+                #logging.info("Worker => " + worker[0] + " has " + str(len(worker[1])) + " connections")
 
 
 def get_fixed_workers_traffic(traffic, workers):
