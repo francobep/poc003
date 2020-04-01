1701f2be206721134e63b6008ebb7a61d570c109
Franco Giovanolli <franco.giovanolli@cba.gov.ar>
2020-04-01 03:31:54 -0300

    .

--- cordinator.py
+++ cordinator.py
@@ -335,6 +335,7 @@ def tcp_sessions(sleeptime=3, lbmode=1, dryrun=False):
 
         total_traffic = total_traffic - total_traffic_a
         fixed_workers_traffic = get_fixed_workers_traffic(total_traffic, total_workers)
+        logging.info("Total Connections: " + str(total_connections))
         logging.info("Total Traffic: " + str(total_traffic))
         logging.info("Total Workers: " + str(total_workers))
         logging.info("Calculating Fixed connections based on total traffic connections divide into total workers...")
